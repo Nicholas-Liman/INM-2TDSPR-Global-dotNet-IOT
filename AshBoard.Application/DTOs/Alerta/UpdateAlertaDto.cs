a00df2904d48eb4a597738fbebaf002f84a56947
Nicholas Lima <nicholas.lima.aero@gmail.com>
2025-05-29 23:04:14 -0300

    Finalização do simulador

--- AshBoard.Application/DTOs/Alerta/UpdateAlertaDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AshBoard.Application.DTOs.Alerta
-{
-    public class UpdateAlertaDto
-    {
-        public string Gravidade { get; set; } = "Verde";
-        public bool IncendioProximo { get; set; }
-    }
-}
